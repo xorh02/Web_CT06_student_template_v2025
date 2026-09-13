774b98e376ca894e2a7c4c3f27dd2289c9965dca
xorh02 <xavierong02@gmail.com>
2026-09-13 08:03:47 +0000

    Sep 13, 2026, 8:03 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,1 +1,42 @@
 // write your codes here
+// write your codes here 
+//            0         1       2
+let list = ["pizza","burger","sushi"];
+function setup(){
+    createCanvas(400,400);
+}
+let yPos =400;
+let lineGap = 60;
+function draw(){
+    background(0);
+  
+    //task3
+    fill(255,255,0);//rgb
+    textSize(24);
+    textAlign(CENTER);
+    for(let i=0; i<list.length;i++){
+         text( (i+1) +". "+list[i], width/2,yPos + i*lineGap);
+     }
+    yPos -= 0.6;
+    if(yPos < -list.length * lineGap){
+        yPos = height;
+    }
+}
+  // //text("what u want to render",x,y)
+    // textSize(20);
+    // text("li chen",50,50);
+    // //age
+    // textSize(67);
+    // text("i am 16 this year",50,100);
+    // //favourtite activity
+    // textSize(14);
+    // text("My favourite activity is eating",50,150);
+    
+    //task2
+    // textSize(20);
+    // textAlign(LEFT);
+    // text("My Favourtie Foods:")
+    // for(let i=0; i<list.length;i++){
+    //     text( (i+1) +". "+list[i], 50,i*30);
+    // }
+    
--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -1,1 +1,43 @@
-// write your codes here
+let storyText =[
+    "A long time ago in a galaxy far",
+    "far away",
+    "It is a period of learning",
+    "Our adventure begins here",
+    "Anything is possible"
+] 
+let yPos = 400;
+let lineGap=60;
+let bgSound;//varname variable name
+let started = false;
+function preload(){
+    bgSound = loadSound('assets/star_wars_theme_8_bit.mp3');
+}
+function setup(){
+    createCanvas(400,400);
+}
+function draw(){
+    background(0);
+    if(started){
+        fill(255,255,0);
+        textSize(24);
+        textAlign(CENTER);
+        for(let i=0; i<storyText.length;i++){
+            text(storyText[i],width/2,yPos +i* lineGap);
+        }
+        yPos -= 0.6;
+        if(yPos <-storyText.length *lineGap){
+            yPos = height;
+        }
+    }else{
+        fill(255,255,0);
+        textSize(24);
+        textAlign(CENTER);
+        text("CLICK To START show",width/2,height/2);
+    }
+}
+function mousePressed(){
+    if(started === false){
+        started = true;
+        bgSound.loop();
+    }
+}
