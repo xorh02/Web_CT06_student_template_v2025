3975aab12bf772ba5b413f7448cd1370b34f55d5
xorh02 <xavierong02@gmail.com>
2026-09-13 08:35:48 +0000

    Sep 13, 2026, 8:35 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -23,7 +23,7 @@ function draw(){
     }
     //Task1
     textSize(32);
-    textAlign(RIGHT)
+    textAlign(LEFT)
     text("Lycus" ,50,50);
     text("8" ,50,100);
     text("Minecraft" ,50,150);
