f16f50b2d8236ef196c454f7ee8f4d9dfe591d56
xorh02 <xavierong02@gmail.com>
2026-09-13 08:46:06 +0000

    Sep 13, 2026, 8:46 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -34,6 +34,6 @@ function draw(){
     textAlign(LEFT);
     text("My favourite Food:",50,50);
     for(let i =0; i<favfood.length;i++){
-        text(favfood[i],50,90)
+        text(favfood[i],50,90 + i)
     }
 }
