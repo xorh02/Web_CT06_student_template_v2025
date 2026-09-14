98b1b48e56154ffc4c28cce207883ac7d8c5ff6a
xorh02 <xavierong02@gmail.com>
2026-09-14 10:26:44 +0000

    Sep 14, 2026, 10:26 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -38,5 +38,9 @@ function mousePressed(){
     interval = setInterval(updateCountdown,1000);
 }
 function updateCountdown(){
-    if(countdown > 0)
+    if(countdown > 0){
+        countdown -= 1;
+    }else{
+        
+    }
 }
