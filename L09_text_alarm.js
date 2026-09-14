2a579cc8806eea46997911aacc2d134934d42a0b
xorh02 <xavierong02@gmail.com>
2026-09-14 10:03:44 +0000

    Sep 14, 2026, 10:03 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,6 +1,6 @@
 // write your codes here
 let x = 200;
-let speed = 0.6;
+let speed = 2;
 function setup(){
     createCanvas(400,400);
 }
@@ -11,6 +11,6 @@ function draw(){
     text("Bounce!",x,200);
     x += speed;
     if(x > width || x<0){
-        
+        speed = -speed;
     }
 }
