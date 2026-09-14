791d8c7c1b2902826b2cad783b41253bd338abf3
xorh02 <xavierong02@gmail.com>
2026-09-14 09:34:32 +0000

    Sep 14, 2026, 9:34 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -57,8 +57,8 @@ function draw(){
         yPos = height
     }
     }else{
-         
-        textSize(32);
+          fill(255,255,0);
+          textSize(32);
           text("Lycus" ,350,50);
     }
 }
